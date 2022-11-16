a5613f9ea3ed6e48606c948fe696dd614efaa80b
Majeed <rahmanmajeed46@gmail.com>
2022-11-16 08:34:34 +0600

    [add] middleware:: for protect routes.

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const secret = '4715aed3c946f7b0a38e6b534a9583628d84e96d10fbc04700770d572af3dce43625dd';
 
-exports.authMiddleware = (req, res, next) => {
+exports.adminMiddleware = (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
     jwt.verify(token, secret, (err, decodedToken) => {
@@ -12,6 +12,25 @@ exports.authMiddleware = (req, res, next) => {
       } else {
         next();
       }
+    });
+  } else {
+    return res
+      .status(401)
+      .json({ message: 'Not authorized, token not available' });
+  }
+};
+
+exports.basicMiddleware = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+      if (decodedToken.role !== 'Basic') {
+        return res.status(401).json({ message: 'Not authorized' });
+      }
+      next();
     });
   } else {
     return res
--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const express = require('express');
 const cookieparser = require('cookie-parser');
 // express in action  to use...
 const app = express();
+
+const { adminMiddleware, basicMiddleware } = require('./Middleware/auth');
+
+const port = 5000;
+app.set('view engine', 'ejs');
+
+// connection instance
+const { connectDB } = require('./db');
+
+connectDB();
+
 // middleware for grant access to fetch data from request body...
 app.use(express.json());
 
 // custom middleware for protect the routes
 app.use(cookieparser());
 
-const port = 5000;
-// connection instance
-const { connectDB } = require('./db');
+app.use('/api/auth', require('./Auth/Route'));
+
+app.get('/admin', adminMiddleware, (req, res) => res.render('admin'));
+app.get('/basic', basicMiddleware, (req, res) => res.render('user'));
 
 const server = app.listen(port, () => console.log(`Server is running ${port} :port`));
-
-connectDB();
 
 // Handling Error
 process.on('unhandledRejection', (err) => {
   console.log(`An error occurred: ${err.message}`);
   server.close(() => process.exit(1));
 });
-
-app.use('/api/auth', require('./Auth/Route'));
